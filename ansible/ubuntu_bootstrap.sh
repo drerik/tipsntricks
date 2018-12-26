7859d39864659d39a38384e25dad40cb5542ff67
Erik Kaareng-Sunde <erik@eriksunde.com>
2018-12-26 22:26:42 +0100

    Added oneliner and default yes to apt-get install

--- ansible/ubuntu_bootstrap.sh
+++ ansible/ubuntu_bootstrap.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
 # From https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html#latest-releases-via-apt-ubuntu
+#
+# Run: curl https://raw.githubusercontent.com/drerik/tipsntricks/master/ansible/ubuntu_bootstrap.sh | sudo bash
+#
+
 apt-get update &&\
 apt-get install software-properties-common &&\
 apt-add-repository --yes --update ppa:ansible/ansible &&\
-apt-get install ansible
+apt-get install -y ansible
